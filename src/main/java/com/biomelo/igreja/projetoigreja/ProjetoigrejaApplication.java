7443236144f5e66512eae52780e52a5765bb908c
Fabio Melo <fabiobiotec17@gmail.com>
2024-02-22 00:06:32 -0300

    Criação do Projeto e conexão com o Mysql

---
+++ src/main/java/com/biomelo/igreja/projetoigreja/ProjetoigrejaApplication.java
@@ -0,0 +1,13 @@
+package com.biomelo.igreja.projetoigreja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoigrejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoigrejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biomelo/igreja/projetoigreja/ProjetoigrejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biomelo.igreja.projetoigreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoigrejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
